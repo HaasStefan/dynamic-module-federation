7cac889ee1d2093df5f503d675148679d81445d5
Stefan Haas <stefan.haas.privat@gmail.com>
2022-10-28 14:52:35 +0200

    Initial commit

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dynamic-module-federation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shell';
+}
